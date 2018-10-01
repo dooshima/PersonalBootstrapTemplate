95f7b54933466ba774ebcf5305a512a90ef32acf
Anger Dooshima Lois <danielladoosh@gmail.com>
2018-10-01 14:47:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function dropdownMenu() {
+		// body...
+	var x = document.getElementById('dropdownClick');
+	if (x.className ==="topnav") {
+		x.className += " responsive";
+		// chage topnav to topnav.responsive
+
+	}else{
+		x.className="topnav";
+
+	}
+
+}
